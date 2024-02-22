e3606ec0824e6ecc4fafd68c83e38df52a86d1c0
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2024-02-22 09:49:09 -0800

    more functions, more tests, more fun.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,95 @@
 use pyo3::prelude::*;
 
+#[pyfunction]
+fn sum(a: isize, b: isize) -> PyResult<isize> {
+    Ok(a + b)
+}
+
+#[pyfunction]
+fn difference(a: isize, b: isize) -> PyResult<isize> {
+    Ok(a - b)
+}
+
+#[pyfunction]
+fn product(a: isize, b: isize) -> PyResult<isize> {
+    Ok(a * b)
+}
+
+#[pyfunction]
+fn quotient(a: isize, b: isize) -> PyResult<isize> {
+    Ok(a / b)
+}
+
+#[pyfunction]
+fn remainder(a: isize, b: isize) -> PyResult<isize> {
+    Ok(a % b)
+}
+
+#[pyfunction]
+fn fibonacci(n: isize) -> PyResult<isize> {
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 0;
+    for _ in 0..n {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    Ok(c)
+}
+
+#[pyfunction]
+fn factorial(n: isize) -> PyResult<isize> {
+    let mut result = 1;
+    for i in 1..n+1 {
+        result *= i;
+    }
+    Ok(result)
+}
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: isize, b: isize) -> PyResult<String> {
-    Ok((a + b).to_string())
+    Ok(sum(a,b)?.to_string())
 }
 
 /// Formats the difference of two numbers as string.
 #[pyfunction]
 fn difference_as_string(a: isize, b: isize) -> PyResult<String> {
-    Ok((a - b).to_string())
+    Ok(difference(a, b)?.to_string())
 }
 
 /// Formats the product of two numbers as string.
 #[pyfunction]
 fn product_as_string(a: isize, b: isize) -> PyResult<String> {
-    Ok((a * b).to_string())
+    Ok(product(a,b)?.to_string())
 }
 
 /// Formats the quotient of two numbers as string.
 #[pyfunction]
 fn quotient_as_string(a: isize, b: isize) -> PyResult<String> {
-    Ok((a / b).to_string())
+    Ok(quotient(a, b)?.to_string())
 }
 
 /// Formats the remainder of two numbers as string.
 #[pyfunction]
 fn remainder_as_string(a: isize, b: isize) -> PyResult<String> {
-    Ok((a % b).to_string())
+    Ok(remainder(a, b)?.to_string())
+}
+
+#[pyfunction]
+fn power_as_string(a: isize, b: isize) -> PyResult<String> {
+    Ok((a.pow(b as u32)).to_string())
+}
+
+#[pyfunction]
+fn fibonacci_as_string(n: isize) -> PyResult<String> {
+    Ok(fibonacci(n)?.to_string())
+}
+
+#[pyfunction]
+fn factorial_as_string(n: isize) -> PyResult<String> {
+    Ok(factorial(n)?.to_string())
 }
 
 /// A Python module implemented in Rust. 
@@ -35,10 +97,20 @@ fn remainder_as_string(a: isize, b: isize) -> PyResult<String> {
 /// else Python will not be able to import the module.
 #[pymodule]
 fn string_sum(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum, m)?)?;
+    m.add_function(wrap_pyfunction!(difference, m)?)?;
+    m.add_function(wrap_pyfunction!(product, m)?)?;
+    m.add_function(wrap_pyfunction!(quotient, m)?)?;
+    m.add_function(wrap_pyfunction!(remainder, m)?)?;
+    m.add_function(wrap_pyfunction!(fibonacci, m)?)?;
+    m.add_function(wrap_pyfunction!(factorial, m)?)?;
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(difference_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(product_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(quotient_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(remainder_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(power_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(fibonacci_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(factorial_as_string, m)?)?;
     Ok(())
 }
--- test.mojo
+++ test.mojo
@@ -2,8 +2,3 @@ from python import Python
 
 fn main() raises:
     let sum_string  = Python.import_module("string_sum")
-    print("1 + 2 = " + sum_string.sum_as_string(1, 2)) 
-    print("1 - 3 = " + sum_string.difference_as_string(1, 3))
-    print("3 * 5 = " + sum_string.product_as_string(3, 5))
-    print("8 / 2 = " + sum_string.quotient_as_string(8, 2))
-    print("1 % 2 = " + sum_string.remainder_as_string(1, 2))
